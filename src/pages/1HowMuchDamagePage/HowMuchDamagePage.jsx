12cc3ddedb59fde1a1e62736ec137a26a13f477a
justintea <justin.tehhy@gmail.com>
2024-03-09 01:23:33 +0800

    feat: 0130

--- src/pages/1HowMuchDamagePage/HowMuchDamagePage.jsx
+++ src/pages/1HowMuchDamagePage/HowMuchDamagePage.jsx
@@ -34,3 +34,35 @@ export default function HowMuchDamagePage() {
     
   </>);
 }
+
+//? save pt
+// import React from 'react';
+// import { Button, Col, Row, Statistic } from 'antd';
+
+// export default function HowMuchDamagePage() {
+
+//   const damageDealt = 15.00;
+
+
+//   return (<>
+  
+//   {/* <h1> how much damage dealt</h1> */}
+    
+//   <Row gutter={16}>
+  
+//     <Col span={25}>
+//       <Statistic title="Damage dealt" value={damageDealt} precision={2} suffix=" wounds" />
+//       <Button
+//         style={{
+//           marginTop: 16,
+//         }}
+//         type="primary"
+//       >
+//         Calculate
+//       </Button>
+//     </Col>
+  
+//   </Row>
+   
+//   </>);
+// }
