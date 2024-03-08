75356a790a53faee6e1cb0350b920347d005faa2
justintea <justin.tehhy@gmail.com>
2024-03-09 04:59:07 +0800

    feat: styling tab width 190 formcol 50%%

--- src/pages/0LandingPage/LandingPage.jsx
+++ src/pages/0LandingPage/LandingPage.jsx
@@ -27,7 +27,7 @@ export default function LandingPage() {
       <Tabs
         activeKey={activeTab}
         onTabClick={(key) => handleTabClick(key)}
-        style={{ width: '180%', }}
+        style={{ width: '190%', }}
         // tabBarGutter={30} 
         centered={true}
 
